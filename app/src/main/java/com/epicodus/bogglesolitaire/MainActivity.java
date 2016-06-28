b0f848b138b75b3af26c861907f69aa88fb0ac67
sm <scottmcintire90@gmail.com>
2016-06-28 10:40:34 -0700

    add consonants and vowels array

--- app/src/main/java/com/epicodus/bogglesolitaire/MainActivity.java
+++ app/src/main/java/com/epicodus/bogglesolitaire/MainActivity.java
@@ -1,13 +1,47 @@
 package com.epicodus.bogglesolitaire;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mNewGameButton;
+    private static final String[] consonants = new String[] {"B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Z", "Y"};
+    private static final String[] vowels = new String[] {"A", "E", "I", "O", "U"};
+
+    private String con1 = consonants[(int)Math.floor(Math.random() * 20)];
+    private String con2 = consonants[(int)Math.floor(Math.random() * 20)];
+    private String con3 = consonants[(int)Math.floor(Math.random() * 20)];
+    private String con4 = consonants[(int)Math.floor(Math.random() * 20)];
+    private String con5 = consonants[(int)Math.floor(Math.random() * 20)];
+
+    private String vowel1 = vowels[(int)Math.floor(Math.random() * 5)];
+    private String vowel2 = vowels[(int)Math.floor(Math.random() * 5)];
+    private String vowel3 = vowels[(int)Math.floor(Math.random() * 5)];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mNewGameButton = (Button) findViewById(R.id.newGameButton);
+        mNewGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NewGameActivity.class);
+                intent.putExtra("con1", con1);
+                intent.putExtra("con2", con2);
+                intent.putExtra("con3", con3);
+                intent.putExtra("con4", con4);
+                intent.putExtra("con5", con5);
+                intent.putExtra("vowel1", vowel1);
+                intent.putExtra("vowel2", vowel2);
+                intent.putExtra("vowel3", vowel3);
+                startActivity(intent);
+
+            }
+        });
     }
 }
--- app/src/main/java/com/epicodus/bogglesolitaire/NewGameActivity.java
+++ app/src/main/java/com/epicodus/bogglesolitaire/NewGameActivity.java
@@ -3,23 +3,26 @@ package com.epicodus.bogglesolitaire;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.util.Log;
+import android.widget.TextView;
 
 public class NewGameActivity extends AppCompatActivity {
-    private Button mNewGameButton;
-
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private TextView mRandomLetters;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game);
-        mNewGameButton = (Button) findViewById(R.id.newGameButton);
-        mNewGameButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, NewGameActivity.class);
-                startActivity(intent);
-            }
-        })
+        Intent intent = getIntent();
+        String con1 = intent.getStringExtra("con1");
+        String con2 = intent.getStringExtra("con2");
+        String con3 = intent.getStringExtra("con3");
+        String con4 = intent.getStringExtra("con4");
+        String con5 = intent.getStringExtra("con5");
+        String vowel1 = intent.getStringExtra("vowel1");
+        String vowel2 = intent.getStringExtra("vowel2");
+        String vowel3 = intent.getStringExtra("vowel3");
+        mRandomLetters = (TextView) findViewById(R.id.randomLetters);
+        mRandomLetters.setText(con1 + con2 + vowel1 + con3 + con4 + vowel2 + con5 + vowel3);
     }
 }
